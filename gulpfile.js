474ef2d88045c40e6ad96a1d384800fc668e7975
git-username <git-email-address>
2014-08-11 22:12:35 -0400

    initial bower,npm, and index.html

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+// Include gulp
+var gulp = require('gulp'); 
+var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
+var clean = require('gulp-clean');
+var karma = require('gulp-karma');
+
+var testFiles = [
+  './app/bower_components/angular*/*.js',
+  './app/bower_components/jquery/dist/*.js',
+  './node_modules/jasmine-jquery/lib/*.js',
+  './app/*.js'
+];
+
+gulp.task('test', function() {
+  // Be sure to return the stream
+  return gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
+gulp.task('autotest', function() {
+  // Be sure to return the stream
+  return gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }))
+});
+
+gulp.task('copy-html-files', function() {
+  gulp.src(['./app/**/*.html', '!./app/index.html'], {base: './app'})
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('usemin', function() {
+  gulp.src('./app/index.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat', rev()],
+      js: [uglify(), rev()]
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app/'
+  });
+});
+
+// Default Task
+gulp.task('default', ['connect']);
+gulp.task('build', ['copy-html-files', 'usemin']);
