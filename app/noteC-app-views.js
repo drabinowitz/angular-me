2cc76aa397b62383b14e93158579c448b1d7ab34
git-username <git-email-address>
2014-08-16 07:51:50 -0400

    added filtering tools for decks and cards

--- app/noteC-app-views.js
+++ app/noteC-app-views.js
@@ -1,1 +1,34 @@
 var viewsModule = angular.module('noteCAppViews',['ngRoute','ngAnimate','noteCLibrary','noteCDirectives']);
+
+viewsModule.filter('with', function() {
+
+  return function(items, field) {
+
+    field = field || '';
+
+    var regex = new RegExp(".*" + field + ".*", "g");
+
+    var result = {};
+
+    angular.forEach(items, function(value, key) {
+
+        if (key.match(regex) || 
+        (value &&
+          typeof value === 'object' &&
+          (
+            (value.hasOwnProperty('description') && value.description.match(regex)) ||
+            (value.hasOwnProperty('content') && value.content.match(regex))
+          )
+        )) {
+
+            result[key] = value;
+
+        }
+
+    });
+
+    return result;
+
+  };
+  
+});
